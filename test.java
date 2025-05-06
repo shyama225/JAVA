b1a2ad2ca82a288e6689c9084027480d0603ce62
shyama225 <shyama2002feb@gmail.com>
2025-05-06 11:25:58 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,9 @@
+import shapes.*;
+public class test{
+public static void main(String[]args){
+Circle obj1=new Circle();
+Triangle obj2=new Triangle();
+System.out.println("Area of Circle:" + obj1.area_c(4));
+System.out.println("Area of Triangle:" + obj2.area_t(4,6));
+}
+}
