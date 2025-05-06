0396ae6277727baacc1199cd1337d7aa111de7ee
shyama225 <shyama2002feb@gmail.com>
2025-05-06 11:25:26 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,7 @@
+package shapes;
+public class Circle{
+public double area_c(double r){
+return 3.14*r*r;
+}
+}
+
---
+++ Triangle.java
@@ -0,0 +1,5 @@
+package shapes;
+public class Triangle{
+public double area_t(double h,double b){
+return 0.5*h*b;
+}}
